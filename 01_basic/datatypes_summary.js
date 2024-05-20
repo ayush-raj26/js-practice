9da754f6fbf6f1c815937d191f1dfd12635a06b2
Ayush Raj <121502079+ayush-raj26@users.noreply.github.com>
2024-05-20 18:57:31 +0000

    memory allocation learned

--- 01_basic/datatypes_summary.js
+++ 01_basic/datatypes_summary.js
@@ -23,3 +23,32 @@ const myFunction = function(){
 // JS is a dynamically typed language, which means that the type of the variable is determined at runtime.  
 
 // Documentation => https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ********************* Memory Allocation ******************
+
+// Primitve Datatypes are stored in the stack memory, while the reference datatypes are stored in the heap memory.
+// Stack (Primitve) ; Heap (Non Primitve)
+// In stack we get copy of the variable, while in heap we get the reference of the variable.
+
+// Eg 1:
+let myName = "Ayush"
+let myAnotherName = myName
+myAnotherName = "Ayush Raj"
+console.log(myName);  // Ayush
+console.log(myAnotherName);  // Ayush Raj
+
+// Eg 2:
+let user1 = {
+    id: "user1@google.com",
+    upi: "user1@ibl"
+
+}
+console.log("User ID before referencing to other variable(object): ", user1.id);  //
+
+let user2 = user1;
+user2.id = "changeid@google.com"
+
+console.log(user1.id);  // changeid@google.com
+console.log(user2.id);  // changeid@google.com
