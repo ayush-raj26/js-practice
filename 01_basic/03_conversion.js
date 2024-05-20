b4a3507eb29fbae40c3c1faf06ba79cdb003fe75
Ayush Raj <121502079+ayush-raj26@users.noreply.github.com>
2024-05-20 16:11:14 +0000

    operations and conversion

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -3,7 +3,7 @@ score = "33ac"
 score = null
 score = undefined
 
-console.log(typeof score);  // String
+// console.log(typeof score);  // String
 
 let scoreInNumber = Number(score)
 // console.log(typeof scoreInNumber);  // Number
@@ -18,8 +18,33 @@ let scoreInNumber = Number(score)
 let isLogged = 1
 let isLoggedIn = Boolean(isLogged)
 
-console.log(isLoggedIn);  // true
+// console.log(isLoggedIn);  // true
 
 // 1=> true; 0 => false; 
 // "" => false
 // "Ayush" => true
+
+
+//  ********************* OPERATIONS *****************
+let value = 3
+let negValue = -value
+// console.log(negValue); // -3
+
+
+let str1 = "hello"
+let str2 = "world"
+str3 = str1 + str2;
+// console.log(str1 + str2);  // helloworld
+// console.log(str3);  // helloworld
+
+console.log(str1 + 3);  // hello3
+console.log(str1 + 3 + 3);  // hello33
+console.log(3 + 3 + str1);  // 6hello
+// whichever data type comes first node js will convert all other data types to that data type if possible
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1, num2, num3
+num1 = num2 = num3 = 3 + 3;
+console.table([num1, num2, num3]);
