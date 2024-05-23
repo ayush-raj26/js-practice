5fd75fb7386aa5e7ea2468e25c02be63e0407ad4
Ayush Raj <121502079+ayush-raj26@users.noreply.github.com>
2024-05-23 08:02:10 +0000

    basic json discussion

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,3 +48,26 @@ console.log(Object.values(tinderUser)); // returns an array of values
 console.log(Object.entries(tinderUser)); // returns an array of arrays of key value pairs
 
 console.log(tinderUser.hasOwnProperty("name")); // returns true if the object has the property
+
+
+
+const course = {
+    courseName: "JavaScript",
+    price: 1000,
+    courseInstructor: "Hitesh",
+}
+
+// Another syntax to extract the property
+const {courseInstructor: instructor} = course;
+console.log(instructor);
+
+
+// In general APIs are just objects
+// EventTarget. of typical JSON file contents
+// {
+//     "name": "Ayush",
+//     "course": "JavaScript",
+//     "courseDureation": "2 months",
+// }
+
+// API can also be in forms of arrays, not necessarily objects
